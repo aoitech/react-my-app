9f45487795f0ddb0b5c04fb3f9b7a30a8a7b8e9e
aoitech <aoi19900722@yahoo.co.jp>
2023-01-28 15:07:42 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+
+
+// export default App;
+
+import React from 'react';
+import HelloMessageFunction from './HelloMessageFunction';
+
+// classコンポーネント(初期の書き方)
+// class HelloMessageClass extends React.Component {
+//   render() {
+//     return <div>Hello redimpulz</div>;
+//   }
+// }
+
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <HelloMessageClass /> */}
+      <HelloMessageFunction />
     </div>
   );
 }
-
-export default App;
---
+++ src/HelloMessageFunction.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// 関数コンポーネント（新しい書き方）
+const HelloMessageFunction = () => {
+  return <div>Hello redimpulz</div>;
+}
+
+export default HelloMessageFunction;
